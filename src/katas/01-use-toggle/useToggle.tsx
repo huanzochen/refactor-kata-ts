e7d922090b517928a9b444727001b3a050b70ad8
ziquanh19 <cy94295@gmail.com>
2026-07-04 20:28:51 +0800

    useToggle

--- src/katas/01-use-toggle/useToggle.tsx
+++ src/katas/01-use-toggle/useToggle.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 
 export function useToggle(initialValue: boolean): [boolean, () => void]{
-    const [open, setOpen] = useState(initialValue);
+    const [value, setOpen] = useState(initialValue);
     const toggle = () => setOpen((v) => !v);
-    return [open, toggle];
+    return [value, toggle];
 }
